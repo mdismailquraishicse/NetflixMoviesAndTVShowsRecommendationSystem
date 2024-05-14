20e8cf36e7c366b06a855e9b57bd88c945ac1abf
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2024-05-14 20:23:00 +0530

    project ready for deployment

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,url_for
 import recommend as rc
 import pickle
 titles = pickle.load(open('./pickles/indices.pkl','rb'))
+result_df = pickle.load(open('./pickles/result_df.pkl','rb'))
+
 
 app = Flask('__name__')
 
 @app.route('/')
 def index():
-    recommendations = rc.recommend('23:59',6)
+    recommendations = rc.recommend('23:59',3)
     return render_template('index.html',
                            titles = titles)
 
@@ -15,9 +17,16 @@ def index():
 @app.route('/recommend',methods=["POST"])
 def recommend():
     rec = request.form['rec']
-    recommend = rc.recommend(rec,6)
+    recommend = rc.recommend(rec,13)
     return render_template("recommendation.html",
-                           recommend=recommend)
+                           recommend=recommend,
+                           titles=list(recommend['title']))
+
+@app.route('/details/<index>')
+def details(index):
+    details=result_df.loc[int(index)]
+    return render_template("details.html",
+                           df=details)
 
 
 
@@ -28,4 +37,4 @@ def recommend():
 
 
 if __name__=='__main__':
-    Flask.run(debug=True)
+    Flask.run(debug=False)
